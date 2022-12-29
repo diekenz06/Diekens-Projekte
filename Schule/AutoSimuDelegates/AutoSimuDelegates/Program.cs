ca4dc91978aca163abddc51198c2116b906f3c62
diekenz06 <200138@studierende.htl-donaustadt.at>
2022-12-29 22:18:03 +0100

    upload

---
+++ Schule/AutoSimuDelegates/AutoSimuDelegates/Program.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5493fad07a6e5060798c05d0b18f55315bbd9d25d3230eeb27f56d3d5c8ba0e5
+size 6689
---
+++ Schule/AutoSimuDelegates/AutoSimuDelegates/Properties/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dd144d9eba165796a8c06e9352cfaf4d4829ae0bafc81482f4b9d6ca6ce4dda8
+size 1569
